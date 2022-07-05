af5bbccd1405e2cb7101036d3d739043d97fdc2f
ameen06 <ameename406@gmail.com>
2022-07-05 10:53:43 +0530

    version 1.0 - google seo

--- src/views/PathNotFound.vue
+++ src/views/PathNotFound.vue
@@ -3,7 +3,7 @@
     <img src="https://img.freepik.com/free-vector/page-found-concept-illustration_114360-1869.jpg?t=st=1653025749~exp=1653026349~hmac=f3771fc4d88df367621fd0f5e9f06fd6f45ae036a9a648249a2dcd05ada22604&w=826"
     alt="404 Page not found image from Freepik" class="w-full lg:max-w-lg">
 
-    <p class="text-3xl lg:text-6xl mt-4 text-blue-700">This page - <span class="font-medium">work in progress</span></p>
+    <p class="text-3xl lg:text-6xl mt-4 text-blue-700">404 <span class="font-medium">page not found</span></p>
     <a href="/" class="">
       <button class="p-2 mt-6 bg-red-500 text-lg text-white rounded-lg font-semibold">Go to Home</button>
     </a>
