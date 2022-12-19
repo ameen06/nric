381f964d02b62546a0f987aef50873fa1e840646
ismail ichu <ismailjamalichu@gmail.com>
2022-12-19 20:31:15 +0530

    image changed and some color and added duration on navigation

--- src/components/basic/navigation.vue
+++ src/components/basic/navigation.vue
@@ -12,31 +12,31 @@
                 <!-- Navigation -->
                 <nav class="hidden md:block">
                     <ul class="flex gap-x-6">
-                        <li class="lg:text-lg font-semibold hover:text-gray-700 "
+                        <li class="lg:text-lg font-semibold hover:text-gray-700 transition-all ease-in-out duration-200 "
                         :class="$route.name === 'Home' ? 'text-green-500' : 'text-gray-500'">
                             <a href="/">Home</a>
                         </li>
-                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 "
+                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 transition-ease-in-out duration-200 "
                          :class="$route.name === 'About' ? 'text-green-500' : 'text-gray-500'">
                             <a href="/about">About</a>
                         </li>
-                        <li v-if="$route.name !== 'Home'" class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 "
+                        <li v-if="$route.name !== 'Home'" class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 transition-ease-in-out duration-200"
                          :class="$route.name === 'Blog' ? 'text-green-500' : 'text-gray-500'">
                             <a href="/blog">News & Events</a>
                         </li>
-                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 "
-                         :class="$route.name === 'Academics' ? 'text-green-500' : 'text-gray-500'">
+                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 transition-ease-in-out duration-200"
+                         :class="$route.name === 'Academics' ? 'text-green-500' : 'text-gray-500'"> 
                             <a href="/academics">Academics</a>
                         </li>
-                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 "
+                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 transition-ease-in-out duration-200"
                          :class="$route.name === 'Admission' ? 'text-green-500' : 'text-gray-500'">
                             <a href="/admission">Admission</a>
                         </li>
-                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 "
+                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 transition-ease-in-out duration-200"
                          :class="$route.name === 'Trust' ? 'text-green-500' : 'text-gray-500'">
                             <a href="/trust">Trust</a>
                         </li>
-                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 "
+                         <li class="lg:text-lg font-semibold hover:text-gray-700 focus:text-green-500 transition-ease-in-out duration-200"
                          :class="$route.name === 'Contact' ? 'text-green-500' : 'text-gray-500'">
                             <a href="/contact">Contact</a>
                         </li>
@@ -49,7 +49,7 @@
                     </div>
         
                     <a href="#" class="hidden md:block">
-                        <button class="px-4 py-2 rounded-full bg-green-600 hover:bg-green-800 text-white">Donate</button>
+                        <button class="px-4 py-2 rounded-full bg-green-600 transition-all duration-200 hover:bg-green-800 text-white">Donate</button>
                     </a>
                     <button class="block md:hidden focus:text-green-500" x-on:click="show = !show" type="button">
                         <i class='bx bx-menu text-3xl'></i>
@@ -64,7 +64,7 @@
             <button class="float-right focus:text-red-400" x-on:click="show = !show" type="button">
                 <i class='bx bx-x text-3xl'></i>
             </button>
-            <ul class="flex flex-col gap-y-6 my-10">
+            <ul class="flex flex-col gap-y-6 my-10 ">
                 <li class="lg:text-xl font-semibold hover:text-gray-700 "
                 :class="$route.name === 'Home' ? 'text-green-500' : 'text-gray-500'">
                     <a href="/">Home</a>
